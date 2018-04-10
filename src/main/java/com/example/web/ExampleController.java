17b65b621815424162567b8057330de2bb8b2dcb
Jochen Pier <jpier@me.com>
2018-04-11 00:16:38 +0200

    add flag for html5valdiation

--- src/main/java/com/example/web/ExampleController.java
+++ src/main/java/com/example/web/ExampleController.java
@@ -1,14 +1,17 @@
 package com.example.web;
 
 import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.example.jwebform_integration.RequestEnvBuilder;
 import com.example.web.forms.ExampleForm;
 import com.example.web.forms.FormcheckerCopyForm;
+
 import jwebform.FormResult;
 import jwebform.themes.common.StartEndRenderer;
 import jwebform.themes.sourcecode.ThemeJavaRenderer;
@@ -20,6 +23,25 @@ public class ExampleController {
 
   private static final Logger log = LoggerFactory.getLogger(ExampleController.class);
 
+  @RequestMapping("/bootstrap2")
+  public String bootstrap2(Model model, HttpServletRequest request) {
+    FormResult formResult = ExampleForm.build().run(new RequestEnvBuilder().of(request));
+    StartEndRenderer renderer = new StartEndRenderer(formResult, "GET", true);
+    model.addAttribute("form", formResult.getView("GET", false));
+    model.addAttribute("startEnd", renderer);
+    if (formResult.isOk()) {
+      model.addAttribute("ok", true);
+    }
+    // FormResult formResult2 = ExampleForm.build("id2").run(new RequestEnv(request));
+    // model.addAttribute("form2", formResult2.getView().getHtml(theme));
+    // if (formResult2.isOk()) {
+    // System.err.println("Everything is fine!2");
+    // }
+
+
+    return "example";
+  }
+
 
   @RequestMapping("/example")
   public String example(Model model, HttpServletRequest request) {
@@ -28,7 +50,7 @@ public class ExampleController {
         new StandardMapper(jwebform.themes.sourcecode.BootstrapTheme.instance(msg -> msg)));
 
     model.addAttribute("form", renderer.render("POST", true));
-    model.addAttribute("form_raw", formResult.getView());
+    model.addAttribute("form_raw", formResult.getView("GET", true));
     if (formResult.isOk()) {
       System.err.println("Everything is fine!");
     }
@@ -49,7 +71,7 @@ public class ExampleController {
     ThemeJavaRenderer renderer = new ThemeJavaRenderer(formResult,
         new StandardMapper(jwebform.themes.sourcecode.BootstrapTheme.instance(msg -> msg)));
     model.addAttribute("form", renderer.render("GET", true));
-    model.addAttribute("form_raw", formResult.getView());
+    model.addAttribute("form_raw", formResult.getView("GET", true));
     if (formResult.isOk()) {
       System.err.println("Everything is fine!");
     }
@@ -64,24 +86,6 @@ public class ExampleController {
   }
 
 
-  @RequestMapping("/bootstrap2")
-  public String bootstrap2(Model model, HttpServletRequest request) {
-    FormResult formResult = ExampleForm.build().run(new RequestEnvBuilder().of(request));
-    StartEndRenderer renderer = new StartEndRenderer(formResult, "GET", true);
-    model.addAttribute("form", formResult.getView());
-    model.addAttribute("startEnd", renderer);
-    if (formResult.isOk()) {
-      model.addAttribute("ok", true);
-    }
-    // FormResult formResult2 = ExampleForm.build("id2").run(new RequestEnv(request));
-    // model.addAttribute("form2", formResult2.getView().getHtml(theme));
-    // if (formResult2.isOk()) {
-    // System.err.println("Everything is fine!2");
-    // }
-
-
-    return "example";
-  }
 
 
 }
