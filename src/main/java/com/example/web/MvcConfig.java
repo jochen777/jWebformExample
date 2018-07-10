b5128d448b0a06588a7172943c5bcb5a561b5665
Jochen Pier <jpier@me.com>
2018-07-10 23:16:56 +0200

    Start with new ViewElement, that builds the only element in templates

--- src/main/java/com/example/web/MvcConfig.java
+++ src/main/java/com/example/web/MvcConfig.java
@@ -5,17 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import com.mitchellbosecke.pebble.spring4.PebbleViewResolver;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
-  @Autowired
-  PebbleViewResolver resolver;
-
-
-  @Bean(name = "viewResolver")
-  public ViewResolver getViewResolver() {
-    return resolver;
-  }
+//  @Autowired
+//  PebbleViewResolver resolver;
+//
+//
+//  @Bean(name = "viewResolver")
+//  public ViewResolver getViewResolver() {
+//    return resolver;
+//  }
 }
