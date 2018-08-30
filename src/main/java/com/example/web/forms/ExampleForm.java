31103463a3ab34eda1d26aab955c0ec0d1216d1c
Jochen Pier <jpier@me.com>
2018-08-30 21:28:03 +0200

    Rename FieldBuilder methods for better readability

--- src/main/java/com/example/web/forms/ExampleForm.java
+++ src/main/java/com/example/web/forms/ExampleForm.java
@@ -2,19 +2,18 @@ package com.example.web.forms;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 import jwebform.Form;
 import jwebform.FormBuilder;
 import jwebform.FormResult;
+import jwebform.field.builder.BuildInType;
 import jwebform.field.builder.FieldBuilder;
 import jwebform.processor.FieldValdationResults;
 import jwebform.processor.LoggingFormResult;
 import jwebform.validation.Criterion;
 import jwebform.validation.FormValidator;
 import jwebform.validation.ValidationResult;
-import jwebform.validation.Validator;
 import jwebform.validation.criteria.Criteria;
 
 import static jwebform.field.builder.BuildInType.*;
@@ -27,36 +26,36 @@ public class ExampleForm {
 
       // @formatter:off
 
-  return FormBuilder.array(
+  return BuildInType.array(
 
     xsrfProtectionForTesting(),
 
     simple(),
 
     text                  ("textInput", "Peter\"Paul").
-      withCriteria        (req).
-      withLabel           ("TextInputLabel"),
+      criteria        (req).
+      label           ("TextInputLabel"),
 
     textDate              ("dateInput", LocalDate.of(2017, 7, 4)).
-      withCriteria        (req).
-      withLabel           ("Please insert date").
-      withHelptext        ("datehelptext"),
+      criteria        (req).
+      label           ("Please insert date").
+      helpText        ("datehelptext"),
 
     text                  ("textInput2", "Peter\"Paul").
-      withCriteria        (req).
-      withLabel           ("TextInputLabel2").
-      withHelptext        ("Help-Text").
-      withPlaceholder     ("Placeholder"),
+      criteria        (req).
+      label           ("TextInputLabel2").
+      helpText        ("Help-Text").
+      placeholder     ("Placeholder"),
 
     select                ("gender", "", new String[] {"m", "f"}, new String[] {"Male", "Female"}).
-      withLabel           ("Gender"),
+      label           ("Gender"),
 
     submit("Submit"),
 
     checkbox              ("chk", true).
-      withCriteria        (req).
-      withLabel           ("chk-label").
-      withHelptext        ("chk_help"),
+      criteria        (req).
+      label           ("chk-label").
+      helpText        ("chk_help"),
 
     label                 ("lbl"),
 
@@ -65,24 +64,24 @@ public class ExampleForm {
     hidden                ("hddn", "hddn-value"),
 
     textArea              ("area", "Area-Prebuild").
-      withCriteria        (req).
-      withLabel           ("Area").
-      withHelptext        ("Area-Help").
-      withPlaceholder     ("Area-Placeholder"),
+      criteria        (req).
+      label           ("Area").
+      helpText        ("Area-Help").
+      placeholder     ("Area-Placeholder"),
 
     number                ("nbr", 5).
-      withCriteria        (req).
-      withLabel           ("nbr-label").
-      withHelptext        ("nbr_help"),
+      criteria        (req).
+      label           ("nbr-label").
+      helpText        ("nbr_help"),
 
     password              ("pssword").
-      withLabel           ("Password"),
+      label           ("Password"),
 
     upload                ("upld").
-      withLabel           ("Upload"),
+      label           ("Upload"),
 
     radio                 ("radio", "1", new String[] {"1", "2"}, new String[] {"yes", "no"}).
-      withLabel          ("Radio")
+      label          ("Radio")
   );
 
 // @formatter:on
